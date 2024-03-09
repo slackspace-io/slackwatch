468b68c869d4ad6caa9e41a3fb9c76fb1867bad3
Jacob Morgan <jtuba05@gmail.com>
2024-03-09 20:44:40 +0100

    set at build time

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 export async function fetchPods(): Promise<Array<{name: string, timeScanned: string}>> {
   const baseUrl = process.env.VITE_API_BASE_URL; // Access the environment variable correctly
-  const response = await fetch(`${baseUrl}/pods`);
+  const response = await fetch(`${baseUrl}/api/pods`);
   if (!response.ok) {
       throw new Error('Failed to fetch pods');
   }
