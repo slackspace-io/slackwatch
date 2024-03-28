4630eb70dccc5be768248de19d80e5d31644f1a1
Jacob Morgan <jtuba05@gmail.com>
2024-03-28 19:22:36 +0100

    Returns a string from workloads

--- backend/src/site/app.rs
+++ backend/src/site/app.rs
@@ -1,73 +1,22 @@
 #![allow(non_snake_case, unused)]
-use crate::models::models::Workload;
+
 use dioxus::prelude::*;
+use dioxus::prelude::ServerFnError;
+use wasm_bindgen_futures::spawn_local;
+use crate::models;
+
+#[server] // For the server-side rendering version
+async fn get_all_workloads() -> Result<String, ServerFnError> {
+    use crate::database::client::return_all_workloads;
+    let workloads = return_all_workloads();
+    Ok(workloads.unwrap().iter().map(|w| w.name.clone()).collect::<Vec<String>>().join(", "))
+}
 
 pub fn App() -> Element {
-    let mut count = use_signal(|| 0);
-    let mut workload: Vec<Workload> = use_signal(|| vec![])();
-    log::info!("App started");
+    let mut workloads = use_resource(get_all_workloads);
     println!("App started");
-    let all_workloads = get_all_workloads();
 
     rsx! {
-        h1 { "High-Five counter: {count}" }
-        button { onclick: move |_| count += 1, "Up high!" }
-        button { onclick: move |_| count -= 1, "Down low!" }
-        h2 { "{workload.len()}"},
-        h2 { "This is a paragraph" }
-        h2 {{ to_owned![all_workloads]}
-         for w in all_workloads.map(|w| w.unwrap()){
-            h2 { "{w.name}" }
-        }
-        },
-        // call get all workload
-        button {
-            onclick: move |_| {
-                to_owned![workload];
-                async move {
-                    if let Ok(workloads) = get_all_workloads().await {
-                        for w in workloads {
-                            println!("workload {:?}", w);
-                            workload.push(w);
-                        }
-                    }
-                }
-            },
-            "Get All Workloads"
-        }
-
-        button {
-            onclick: move |_| {
-                to_owned![count];
-                async move {
-                    if let Ok(new_count) = double_server(count()).await {
-                        count.set(new_count);
-                    }
-                }
-            },
-            "Double"
-        }
-
-
-    }
+        "server data is {workloads():?}"
 }
-
-#[server]
-async fn get_all_workloads() -> Result<Vec<Workload>, ServerFnError> {
-    // Perform some expensive computation or access a database on the server
-    //    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-    print!("Getting all workloads");
-    use crate::database::client::return_all_workloads;
-    let workloads = return_all_workloads()?;
-    Ok(workloads)
-}
-
-#[server]
-async fn double_server(number: i32) -> Result<i32, ServerFnError> {
-    // Perform some expensive computation or access a database on the server
-    //    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-    log::info!("Doubling {number}");
-    let result = number * 2;
-    println!("server calculated {result}");
-    Ok(result)
 }
