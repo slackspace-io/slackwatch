7a6c555b28a98b26b613e75726104acd0834e719
Jacob Morgan <jtuba05@gmail.com>
2024-03-29 20:22:25 +0100

    actually iterating

--- backend/src/site/app.rs
+++ backend/src/site/app.rs
@@ -11,21 +11,48 @@ async fn get_all_workloads() -> Result<String, ServerFnError> {
     use crate::database::client::return_all_workloads;
     let workloads = return_all_workloads();
     Ok(workloads.unwrap().iter().map(|w| w.name.clone()).collect::<Vec<String>>().join(", "))
+
+//    Ok(workloads.unwrap().iter().map(|w| w.name.clone()).collect::<Vec<String>>().join(", "))
 }
 
 pub fn App() -> Element {
     let mut workloads = use_resource(get_all_workloads);
     let mut all = use_resource(get_all);
     println!("App started");
-
-    rsx! {
-        "server data is {workloads():?}"
-        div {}
-        "server data is {all():?}"
-        div { {all().map(|w| rsx! { div {"{w:?}"}})}}
-
-
+    rsx! {All{}}
+//    rsx! {
+//        "server data is {workloads():?}"
+//        div {}
+//        "server data is {all():?}"
+//        div { {all().map(|w| rsx! { div {"{w:?}"}})}}
+//
+//
+//}
 }
+
+#[component]
+fn All() -> Element {
+    let workloads = use_server_future(get_all)?;
+    match workloads() {
+        Some(Ok(workloads)) => {
+            rsx! {
+                div {"server data is {workloads:?}" }
+                div { {workloads.iter().map(|w| rsx! { div {"{w.name}"} })}
+                    }
+            }
+        },
+        Some(Err(err)) => {
+            rsx! {
+                div { "Error: {err}" }
+            }
+        },
+        _ => {
+            rsx! {
+                div { "Loading..." }
+            }
+
+        }
+    }
 }
 
 
