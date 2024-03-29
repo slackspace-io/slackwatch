2567539b358f09e809154a0ebdef32e0f7ca7179
Jacob Morgan <jtuba05@gmail.com>
2024-03-29 07:29:55 +0100

    works for full data too

--- backend/src/site/app.rs
+++ backend/src/site/app.rs
@@ -4,6 +4,7 @@ use dioxus::prelude::*;
 use dioxus::prelude::ServerFnError;
 use wasm_bindgen_futures::spawn_local;
 use crate::models;
+use crate::models::models::Workload;
 
 #[server] // For the server-side rendering version
 async fn get_all_workloads() -> Result<String, ServerFnError> {
@@ -14,9 +15,21 @@ async fn get_all_workloads() -> Result<String, ServerFnError> {
 
 pub fn App() -> Element {
     let mut workloads = use_resource(get_all_workloads);
+    let mut all = use_resource(get_all);
     println!("App started");
 
     rsx! {
         "server data is {workloads():?}"
+        div {}
+        "server data is {all():?}"
+
 }
+}
+
+
+#[server]
+async fn get_all() -> Result<Vec<Workload>, ServerFnError> {
+    use crate::database::client::return_all_workloads;
+    let workloads = return_all_workloads();
+    Ok(workloads.unwrap())
 }
