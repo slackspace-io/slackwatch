21037f577be3db3eee50ef7a9140c72ffd738ca9
Jacob Morgan <jtuba05@gmail.com>
2024-04-02 17:35:42 +0200

    working button, for single entry

--- backend/src/site/app.rs
+++ backend/src/site/app.rs
@@ -6,6 +6,22 @@ use wasm_bindgen_futures::spawn_local;
 use crate::models;
 use crate::models::models::Workload;
 
+
+#[derive(Routable, Clone)]
+#[rustfmt::skip]
+enum Route {
+        #[route("/")]
+        Home {},
+    }
+
+
+
+#[derive(PartialEq, Clone,Props)]
+struct WorkloadCardProps {
+    workload: Workload,
+}
+
+
 #[server] // For the server-side rendering version
 async fn get_all_workloads() -> Result<String, ServerFnError> {
     use crate::database::client::return_all_workloads;
@@ -15,9 +31,66 @@ async fn get_all_workloads() -> Result<String, ServerFnError> {
 //    Ok(workloads.unwrap().iter().map(|w| w.name.clone()).collect::<Vec<String>>().join(", "))
 }
 
+
+#[component]
+fn Home() -> Element {
+    let workloads = use_server_future(get_all)?;
+    match workloads() {
+        Some(Ok(workloads)) => {
+            rsx! {
+                WorkloadCard{workload: workloads[0].clone()}
+            }
+        },
+        Some(Err(err)) => {
+            rsx! { div { "Error: {err}" } }
+        },
+        _ => {
+            rsx! { div { "Loading..." } }
+        }
+
+    }
+}
+
+#[component]
+fn WorkloadCard(props: WorkloadCardProps) -> Element {
+    let data = use_signal(|| {props.workload.clone()});
+    rsx! {
+        div {
+            class: if props.workload.update_available == models::models::UpdateStatus::Available {
+                "workload-card-update-available"
+            } else {
+                "workload-card"
+            },
+            div { class: "workload-name", "{props.workload.name}" },
+            button {onclick: move |_| {
+                to_owned![data, props.workload];
+                async move {
+                    if let Ok(_) = update_workload(data()).await {
+                        println!("Updated");
+                    }
+                }
+            },
+                "UpdateNow"},
+            div { class: "workload-namespace", "Namespace: {props.workload.namespace}" },
+            div { class: "workload-version", "Current Tag {props.workload.current_version}" },
+            div { class: "workload-image", "Image: {props.workload.image}" },
+            div { class: "workload-last-scanned", "Last Scanned: {props.workload.last_scanned}" },
+            if props.workload.update_available == models::models::UpdateStatus::Available {
+                div { class: "workload-update-available", "Update Available" }
+            }
+        }
+    }
+}
+
 pub fn App() -> Element {
     println!("App started");
-    rsx! { All {} }
+    rsx! { Router::<Route> {} }
+    //rsx!{
+    //    div {
+    //        "App"
+    //    }
+    //}
+    //rsx! { All {} }
 //    rsx! {
 //        "server data is {workloads():?}"
 //        div {}
@@ -29,6 +102,25 @@ pub fn App() -> Element {
 }
 
 
+#[component]
+fn RefreshAll() -> Element {
+    let refresh = use_server_future(refresh_all)?;
+    match refresh() {
+        Some(Ok(_)) => {
+            rsx! { div { "Refreshed" } }
+        },
+        Some(Err(err)) => {
+            rsx! { div { "Error: {err}" } }
+        }
+        _ => rsx! { div { "Loading..." } }
+    }
+}
+
+
+#[component]
+fn NotFound() -> Element {
+    rsx! { div { "Not Found" } }
+}
 
 
 #[component]
@@ -56,9 +148,9 @@ fn All() -> Element {
                                 div { class: "workload-update-available", "Update Available" }
                             }
                             }
-                            }
-                        
-                        )}
+                            },
+
+                        )},
                     }
                 }
             }
@@ -71,6 +163,23 @@ fn All() -> Element {
 
         }
     }
+}
+
+#[server]
+async fn update_workload(workload: Workload) -> Result<(), ServerFnError> {
+    log::info!("update_workload: {:?}", workload);
+    println!("update_workload");
+    use crate::services::workloads::update_single_workload;
+    let result = update_single_workload(workload).await;
+    Ok(result.unwrap())
+ //   Ok(result.unwrap())
+}
+
+#[server]
+async fn refresh_all() -> Result<(), ServerFnError> {
+    use crate::services::workloads::fetch_and_update_all_watched;
+    let result = fetch_and_update_all_watched().await;
+    Ok(result.unwrap())
 }
 
 
