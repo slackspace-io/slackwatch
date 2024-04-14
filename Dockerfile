5a85953ca82d840c8bda3969ef86d6e28f1b4653
Jacob Morgan <3956959+slackspace-io@users.noreply.github.com>
2024-04-14 22:43:11 +0200

    Introduce caching for dockerfile (#10)
    * Update Dockerfile to optimize layer caching by separate copying of Cargo files and source code
    
    * Update Dockerfile to correct slackwatch binary path
    
    * Update Docker build-push-action to v5 and enable cache in dev-build workflow
    
    * Update Docker build-push-action to v5 and enable cache in dev-build workflow
    
    * Update Docker build-push-action to v5 and enable cache in dev-build workflow
    
    * Update GitHub Actions workflow to use latest checkout and buildx actions, and enhance caching strategy
    
    * Rename GitHub workflow for clarity on dev builds
    
    * Remove unused file, also testing build time from changes
    
    * Update Docker cache settings in GitHub Actions dev-build workflow
    
    * Remove commented out code and clean up main function in main.rs
    
    * Move dioxus-cli installation before copying source files in Dockerfile
    
    * Move dioxus-cli installation before copying source files in Dockerfile
    
    * Enable Docker build caching for Slackwatch workflow

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM rust:1.77.1 as builder
 WORKDIR /app
-COPY . .
 RUN cargo install dioxus-cli@0.5.0
+COPY Cargo.toml Cargo.lock ./
+COPY src ./src
 RUN  dx build --platform fullstack --release
 
 FROM rust:1.77.1
 #RUN apt-get update && rm -rf /var/lib/apt/lists/*
 #Copy all files from the builder
 WORKDIR /app
-COPY --from=builder /app/slackwatch /app/slackwatch
+COPY --from=builder /app/target/release/slackwatch /app/slackwatch
 EXPOSE 8080
 CMD ["/app/slackwatch/slackwatch"]
--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,6 @@ use dioxus::prelude::*;
 use log::info;
 use crate::config::Settings;
 
-//#[cfg(feature = "server")]
-//use k8s_openapi::merge_strategies::list::set;
-//use std::env;
-//
 mod config;
 mod database;
 mod gitops;
@@ -35,22 +31,17 @@ mod site;
 #[tokio::main]
 async fn  main() {
     println!("Hello, world!");
-    //Logging and env variables
     env::set_var("RUST_LOG", "info");
     env_logger::init();
-    //  dotenv::dotenv().ok();
-    // Load Configurations
      let settings = Settings::new().unwrap_or_else(|err| {
          log::error!("Failed to load settings: {}", err);
          panic!("Failed to load settings: {}", err);
      });
      log::info!("Starting up");
      log::info!("Loading configuration {:?}", settings);
-    //create table if not exist
     #[cfg(feature = "server")]
     use crate::database::client::create_table_if_not_exist;
     create_table_if_not_exist().unwrap();
-    //working tokio stuff
     tokio::task::spawn(services::scheduler::run_scheduler(settings.clone()));
     let mut config = dioxus::fullstack::Config::new();
 
@@ -59,12 +50,9 @@ async fn  main() {
         config = config.addr(std::net::SocketAddr::from(([0, 0, 0, 0], 8080)));
     }
 
-// Launch the app with the custom menu
     let site = std::thread::spawn(|| LaunchBuilder::new().with_cfg(config).launch(App));
-    //let site = std::thread::spawn(|| launch(App));
     log::info!("Started logger");
     println!("Started print");
-    //let _ = web::exweb::site();
     site.join().unwrap();
 }
 
